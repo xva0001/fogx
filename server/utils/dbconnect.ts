b814fbf48e6b30146957fe0e36c19d128b67e0af
xva0001 <aaron40555@gmail.com>
2025-01-19 09:41:06 +0800

    modified:   server/utils/dbconnect.ts

--- server/utils/dbconnect.ts
+++ server/utils/dbconnect.ts
@@ -2,13 +2,13 @@ import mongoose from "mongoose"
 
 export const mongodb_conn_fn = async () => {
     try {
-        const uri = "mongodb+srv://msgfog:hO1Kwgq8esPN1Loe@user.a32p7.mongodb.net/?retryWrites=true&w=majority&appName=user";
-        //const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+        const uri = process.env.MONGODB_USER_DB;
+        if (uri==undefined) {
+            throw Error("undefine mongodb uri")
+        }
         const db_conn = await mongoose.connect(uri, { serverApi: { version: "1", strict: true, deprecationErrors: true } })
-
         console.log("connection ok")
         return db_conn
-
     }
     catch (e) {
         console.log(e);
