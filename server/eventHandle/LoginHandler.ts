f232cafc7036692a8600bd3c63069c4444847aa9
ChrIS230020646 <160588100+ChrIS230020646@users.noreply.github.com>
2025-03-27 18:21:11 +0800

    2FA UPDATE
    2FA UPDATE

--- server/eventHandle/LoginHandler.ts
+++ server/eventHandle/LoginHandler.ts
@@ -143,7 +143,7 @@ export const loginEvent = async (event: H3Event) => {
                         sha3_256: user.sha3_256,
                         sha3_384: user.sha3_384,
                         createdDate: user.createdDate,
-                        lastestLoginDate: user.lastestLoginDate,
+                        //lastestLoginDate: user.lastestLoginDate,
                         keyOf2FA: user.keyOf2FA, //share
                         backupCode: user.backupCode,
                         username: user.username,
